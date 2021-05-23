83717adc4118bf3634d1c024a38653f19e6e6f87
Allan Wang <16047959+Valeesi@users.noreply.github.com>
2021-05-23 12:02:06 +0200

    Find all contests starting at 12:00 UTC

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import urllib
 import urllib.request
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -6,17 +5,23 @@ from bs4 import BeautifulSoup
 url = "https://atcoder.jp/contests/archive?page="
 
 dates = []
+dates_st = []
 for num in range(1, 8):
     req = urllib.request.urlopen(url + str(num))
     data = BeautifulSoup(req, "html.parser")
     for row in data.find_all('tr'):
         asd = 0
         if row.find('td') is not None:
-            dates.append(datetime.strptime(row.find_all('td')[0].text, '%Y-%m-%d %H:%M:%S%z'))
-
-    # if (row.find_all('td')[0] !== undefined):
-
+            dt = datetime.strptime(row.find_all('td')[0].text, '%Y-%m-%d %H:%M:%S%z');
+            dates.append(dt)
+            dates_st.append(row.find_all('td')[0].text)
     print('#%d done' % num)
 
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
+counter = 0
+counterYear = 0
+for ele in dates_st:
+    if "21:00" in ele:
+        counter += 1
+print('Total count: %d' % counter)
+print('Total: %d' % len(dates_st))
+print('Percentage: %f' % (counter / len(dates_st) * 100))
